cbf750807f06d3f2ac5526b01b419d7e2c6fc734
Natalia Prieto <natyprietoc@hotmail.com>
2021-02-17 16:40:29 -0500

    additions #5: *added some divs to the HTML *added some styles to the span and the buttons *added the function ocultar *added some changes to the function enviar

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,9 @@
 // const cambioInput = document.querySelectorAll('input')[0].placeholder = 'Error'
 
+function ocultar(){
+  document.getElementById('hideName').style.display = "none";
+}
+
 const enviar = () => {
   const cambioInput = document.querySelectorAll(".input");
   console.log(cambioInput);
@@ -8,8 +12,9 @@ const enviar = () => {
     if (element.value != "") {
       // let validacion =
     } else {
-      element.placeholder = "";
-      element.classList.add("is-invalid");
+      element.placeholder = element.id=="idEmail" ? "email@example.com" : "" ;           
+      element.classList.add("is-invalid");  
+      element.classList.add("id-email");    
     }
   });
 };
