4fa0f4cab2db0aeca50d99a28774cebc44fa2478
Natalia Prieto <natyprietoc@hotmail.com>
2021-02-18 16:08:14 -0500

    additions #5: *changed some id and remove some class *added the style invalid and valid to the validation *changes with DOM

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,20 +1,42 @@
 // const cambioInput = document.querySelectorAll('input')[0].placeholder = 'Error'
 
-function ocultar(){
-  document.getElementById('hideName').style.display = "none";
-}
+ const enviar = () => {
+   const cambioInput = document.querySelectorAll(".input");
+  //  const nameInput = document.getElementById('idName');
+   console.log(cambioInput);
+   var patron = /^[a-z]{3,16}$/
 
-const enviar = () => {
-  const cambioInput = document.querySelectorAll(".input");
-  console.log(cambioInput);
-
-  cambioInput.forEach((element) => {
-    if (element.value != "") {
-      // let validacion =
-    } else {
-      element.placeholder = element.id=="idEmail" ? "email@example.com" : "" ;           
-      element.classList.add("is-invalid");  
-      element.classList.add("id-email");    
-    }
-  });
+   cambioInput.forEach((element) => {
+       if (element.value != "" ) {
+         if (element.id=="idName") {                               
+           element.classList.add("is-valid");
+           document.getElementById('spN').classList.add("hideProp")
+         } else {
+          element.classList.add("is-invalid");
+         }
+     } else {
+       element.placeholder = element.id=="idEmail" ? "email@example.com" : "" ;           
+       element.classList.add("is-invalid");  
+       element.classList.add("id-email");
+       document.getElementById('spN').innerHTML = "First Name cannot be empty";
+       document.getElementById('spL').innerHTML = "Last Name cannot be empty";
+       document.getElementById('spE').innerHTML = "Looks like this is not an email"       
+       document.getElementById('spP').innerHTML = "Password cannot be empty";     
+     }
+   });
 };
+
+// const nameInput = document.getElementById('idName')
+// const lastName = document.getElementById('idLast')
+// const email = document.getElementById('idEmail')
+// const Password = document.getElementById('idPassword')
+
+// const enviar = () => {
+//   if ( nameInput.value != "" ) {
+//     return nameInput.classList.add("is-valid");
+//   } else {
+//     nameInput.classList.add("is-invalid");
+//     nameInput.placeholder =  "" ;
+//     document.getElementById('spN').innerHTML = "First Name cannot be empty"; 
+//   }
+// }
