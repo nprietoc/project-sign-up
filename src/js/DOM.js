86f03a4ec44147705cf16d9de9a86b5fe5801884
Sammy Guttman <1449@holbertonschool.com>
2021-02-18 18:18:26 -0500

    Arreglo condicional expresiones regulares

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,29 +1,32 @@
 // const cambioInput = document.querySelectorAll('input')[0].placeholder = 'Error'
 
- const enviar = () => {
-   const cambioInput = document.querySelectorAll(".input");
+const enviar = () => {
+  const cambioInput = document.querySelectorAll(".input");
   //  const nameInput = document.getElementById('idName');
-   console.log(cambioInput);
-   var patron = /^[a-z]{3,16}$/
+  console.log(cambioInput);
+  var patron = /^[a-z]{3,16}$/;
 
-   cambioInput.forEach((element) => {
-       if (element.value != "" ) {
-         if (element.id=="idName") {                               
-           element.classList.add("is-valid");
-           document.getElementById('spN').classList.add("hideProp")
-         } else {
+  cambioInput.forEach((element) => {
+    if (element.value != "") {
+      if (element.id == "idName") {
+        if (patron.test(element.value)) {
+          element.classList.add("is-valid");
+          document.getElementById("spN").classList.add("hideProp");
+        } else {
           element.classList.add("is-invalid");
-         }
-     } else {
-       element.placeholder = element.id=="idEmail" ? "email@example.com" : "" ;           
-       element.classList.add("is-invalid");  
-       element.classList.add("id-email");
-       document.getElementById('spN').innerHTML = "First Name cannot be empty";
-       document.getElementById('spL').innerHTML = "Last Name cannot be empty";
-       document.getElementById('spE').innerHTML = "Looks like this is not an email"       
-       document.getElementById('spP').innerHTML = "Password cannot be empty";     
-     }
-   });
+        }
+      }
+    } else {
+      element.placeholder = element.id == "idEmail" ? "email@example.com" : "";
+      element.classList.add("is-invalid");
+      element.classList.add("id-email");
+      document.getElementById("spN").innerHTML = "First Name cannot be empty";
+      document.getElementById("spL").innerHTML = "Last Name cannot be empty";
+      document.getElementById("spE").innerHTML =
+        "Looks like this is not an email";
+      document.getElementById("spP").innerHTML = "Password cannot be empty";
+    }
+  });
 };
 
 // const nameInput = document.getElementById('idName')
@@ -37,6 +40,6 @@
 //   } else {
 //     nameInput.classList.add("is-invalid");
 //     nameInput.placeholder =  "" ;
-//     document.getElementById('spN').innerHTML = "First Name cannot be empty"; 
+//     document.getElementById('spN').innerHTML = "First Name cannot be empty";
 //   }
 // }
